8cd8053b3fe28570f8029694a5d9849825addc93
Manh Cao <manhcao@192.168.0.100>
2024-09-13 13:21:36 +0700

    init repo

--- merge-config.js
+++ merge-config.js
@@ -2,37 +2,39 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 
-const protocolsPath = path.join(__dirname, 'protocols');
-const combinedConfigPath = path.join(__dirname, 'config.json');
+async function run() {
+  const protocolsPath = path.join(__dirname, 'protocols');
+  const combinedConfigPath = path.join(__dirname, 'config.json');
 
-const protocolIds = fs.readdirSync(protocolsPath, { withFileTypes: true })
-  .filter(dirent => dirent.isDirectory()) // Filter only directories
-  .filter(dirent => fs.existsSync(path.join(protocolsPath, dirent.name, 'config.json'))) // Check if config.json exists
-  .map(dirent => dirent.name);
+  const protocolIds = fs.readdirSync(protocolsPath, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory()) // Filter only directories
+    .filter(dirent => fs.existsSync(path.join(protocolsPath, dirent.name, 'config.json'))) // Check if config.json exists
+    .map(dirent => dirent.name);
 
-const data = {protocols: []};
+  const data = {protocols: []};
 
-for (const protocolId of protocolIds) {
-  const configPath = path.join(protocolsPath, protocolId, 'config.json');
-  const protocolConfigStr = fs.readFileSync(configPath, 'utf8');
-  const protocolConfig = {
-    id: protocolId,
-    ...JSON.parse(protocolConfigStr)
+  for (const protocolId of protocolIds) {
+    const configPath = path.join(protocolsPath, protocolId, 'config.json');
+    const protocolConfigStr = fs.readFileSync(configPath, 'utf8');
+    const protocolConfig = {
+      id: protocolId,
+      ...JSON.parse(protocolConfigStr)
+    }
+
+    const {icon} = protocolConfig;
+
+    const iconPath = path.join(__dirname, `protocols/${protocolId}/${icon}`);
+    protocolConfig.hash = await createMD5(iconPath);
+
+    data.protocols.push(protocolConfig);
   }
 
-  const {icon} = protocolConfig;
-
-  const iconPath = path.join(__dirname, `protocols/${protocolId}/${icon}`);
-  protocolConfig.hash = await createMD5(iconPath);
-
-  data.protocols.push(protocolConfig);
+  fs.writeFile(combinedConfigPath, JSON.stringify(data, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing to file', err);
+    }
+  });
 }
-
-fs.writeFile(combinedConfigPath, JSON.stringify(data, null, 2), (err) => {
-  if (err) {
-    console.error('Error writing to file', err);
-  }
-});
 
 function createMD5(filePath) {
   return new Promise((res, rej) => {
@@ -47,3 +49,5 @@ function createMD5(filePath) {
     });
   })
 }
+
+void run();
