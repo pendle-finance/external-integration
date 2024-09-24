1bc186976db478a6bb014de9a585e4d4e448f520
Manh Cao <manhcao@Manhs-MacBook-Pro-2.local>
2024-09-24 11:27:26 +0700

    update kebab case check

--- validate-config.js
+++ validate-config.js
@@ -7,7 +7,7 @@ function isValidEthereumAddress(address) {
 }
 
 function isKebabCase(str) {
-  const kebabCaseRegex = /^[a-z]+(-[a-z]+)*$/;
+  const kebabCaseRegex = /^[a-z0-9]+(-[a-z0-9]+)*$/;
   return kebabCaseRegex.test(str);
 }
 
