dba2cb8720b6a701320060548ee71a1543d9b99e
manh-pendle <155348284+manh-pendle@users.noreply.github.com>
2024-11-25 13:47:51 +0700

    add capped characters validation for description (#71)
    Co-authored-by: Manh Cao <manhcao@192.168.0.101>

--- const.js
+++ const.js
@@ -1,6 +1,9 @@
 // categories need to be lowercase strings
 const PROTOCOL_CATEGORIES = ['money market', 'yield strategy', 'liquid locker', 'others'];
 
+const DESCRIPTION_MAXIMUM_CHARACTERS = 120;
+
 module.exports = {
   PROTOCOL_CATEGORIES,
+  DESCRIPTION_MAXIMUM_CHARACTERS,
 }
